4c63ab5b6c6453b804e46819db3b6145f8ee7a49
Binod Dahal <60723232+bdaha@users.noreply.github.com>
2021-12-04 14:35:08 -0500

    Database Migrations

---
+++ AlbumList/Controller/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AlbumList.Models;
+
+namespace AlbumList.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AlbumContext context { get; set; }
+
+        public HomeController(AlbumContext ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult Index()
+        {
+         var albums = context.Albums;
+            return View(albums);
+        }
+    }
+}
--- AlbumList/Startup.cs
+++ AlbumList/Startup.cs
@@ -1,6 +1,8 @@
+using AlbumList.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,33 +25,30 @@ namespace AlbumList
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllersWithViews();
+            services.AddDbContext<AlbumContext>(options =>
+               options.UseSqlServer(
+                   "Server=(localdb)\\mssqllocaldb;Database=AlbumList;Trusted_Connection=True;MultipleActiveResultSets=true"));
+            services.AddRouting(options =>
+            {
+                options.LowercaseUrls = true;
+                options.AppendTrailingSlash = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
+            app.UseDeveloperExceptionPage();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
