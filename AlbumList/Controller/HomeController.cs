3d89dde319c16f926e3614760cdac7e04467c482
Binod Dahal <60723232+bdaha@users.noreply.github.com>
2021-12-05 18:51:44 -0500

    changes to files

--- AlbumList/Controller/HomeController.cs
+++ AlbumList/Controller/HomeController.cs
@@ -11,6 +11,20 @@ namespace AlbumList.Controllers
 {
     public class HomeController : Controller
     {
+<<<<<<< HEAD
         
+=======
+        private AlbumContext context { get; set; }
+
+        public HomeController(AlbumContext ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult Index()
+        {
+         var albums = context.Albums;
+            return View(albums);
+        }
+>>>>>>> ConnectionString
     }
 }
---
+++ AlbumList/Migrations/20211203204801_Initial.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AlbumList.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Album",
+                columns: table => new
+                {
+                    AlbumId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Year = table.Column<int>(type: "int", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Album", x => x.AlbumId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Album",
+                columns: new[] { "AlbumId", "Name", "Year" },
+                values: new object[] { 1, "Example Album", 2020 });
+
+            migrationBuilder.InsertData(
+                table: "Album",
+                columns: new[] { "AlbumId", "Name", "Year" },
+                values: new object[] { 2, "Example Album 2", 2021 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Album");
+        }
+    }
+}
